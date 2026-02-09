c335c110cd90d65bc914ab3c3c77b829c74170e2
Lucas-code297 <lucash7z777@gmail.com>
2026-02-09 00:32:50 -0300

    ex008 - Conversor de Medidas

---
+++ ex008.py
@@ -0,0 +1,9 @@
+# km hm dam m dm cm mm
+metros = float(input('Digite uma distância em metros: '))
+print(f'A medida de {metros}m corresponde a: ')
+print(f'{metros / 1000}km.')
+print(f'{metros / 100}hm.')
+print(f'{metros / 10}dam.')
+print(f'{metros * 10:.0f}dm.')
+print(f'{metros * 100:.0f}cm.')
+print(f'{metros * 1000:.0f}mm.')

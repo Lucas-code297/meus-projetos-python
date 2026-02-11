c6ac1b4ed1574bb0b3c60a77d8ebe161b065d0f8
Lucas-code297 <lucash7z777@gmail.com>
2026-02-11 19:53:17 -0300

    ex028 - Jogo de Adivinhação v.1.0.

--- Exercícios/ex028.py
+++ Exercícios/ex028.py
@@ -1,9 +1,13 @@
 from time import sleep
 from random import randint
 print('Vamos jogar um jogo?')
+sleep(1)
 print('Irei pensar em um número entre 0 e 5.')
+sleep(2)
 print('Você deverá tentar adivinhar...')
+sleep(2)
 print('Direi no final se ganhou ou perdeu.')
+sleep(2)
 computador = randint(0, 5)
 jogador = int(input('Digite um número entre 0 e 5: '))
 if computador == jogador:

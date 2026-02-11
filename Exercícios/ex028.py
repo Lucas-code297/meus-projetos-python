664d8e63ef16b729a9d753e94e1747239ba85b30
Lucas-code297 <lucash7z777@gmail.com>
2026-02-11 19:26:19 -0300

    ex028 - Jogo de adivinhação v.1.0

--- Exercícios/ex028.py
+++ Exercícios/ex028.py
@@ -1,17 +1,12 @@
-# fazer um programa que faça o computador "pensar" em um número inteiro entre 0 e 5 e peça para o usuário
-# tentar adivinhar o número escolhido pelo pc
-# o programa deve mostrar na tela se o usuário venceu ou perdeu
-from random import randint
 from time import sleep
-print('Vou pensar em um número entre 0 e 5!')
-sleep(2)
-print('Você deve adivinhar que número escolhi, direi se venceu ou perdeu!')
-sleep(2)
-num = int(input('Digite um número de 0 a 5: '))
-escolhido = randint(0, 5)
-print('Pensando...')
-sleep(3)
-if num == escolhido:
-    print(f'PARABÉNS! O número escolhido foi {escolhido}, você venceu o jogo!')
-else:
-    print(f'Haha! Eu te venci, o número escolhido foi {escolhido}!')
+from random import randint
+print('Vamos jogar um jogo?')
+print('Irei pensar em um número entre 0 e 5.')
+print('Você deverá tentar adivinhar...')
+print('Direi no final se ganhou ou perdeu.')
+computador = randint(0, 5)
+jogador = int(input('Digite um número entre 0 e 5: '))
+if computador == jogador:
+    print(f'PARABÉNS! Você me venceu!')
+if computador != jogador:
+    print(f'GANHEI! Você escolheu {jogador} e eu escolhi {computador}!')

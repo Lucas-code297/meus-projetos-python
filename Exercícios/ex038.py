a922b21ca381aea79a80f1657b39306f6256952e
Lucas-code297 <lucash7z777@gmail.com>
2026-02-12 21:04:04 -0300

    ex038 - Comparando valores

---
+++ Exercícios/ex038.py
@@ -0,0 +1,9 @@
+a = int(input('Digite um número inteiro: '))
+b = int(input('Digite outro valor: '))
+if a > b:
+    print('O primeiro valor é maior.')
+if a < b:
+    print('O segundo valor é maior.')
+else:
+    if a == b:
+        print('Não há valor maior, ambos são iguais.')

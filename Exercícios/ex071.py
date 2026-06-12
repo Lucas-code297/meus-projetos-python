db946409c479e74778766e57299954a5a33b0a40
Lucas-code297 <lucash7z777@gmail.com>
2026-06-12 17:31:40 -0300

    salvando algumas alteracoes

--- Exercícios/ex071.py
+++ Exercícios/ex071.py
@@ -6,21 +6,37 @@ cedula_c = 50
 cedula_v = 20
 cedula_d = 10
 cedula_u = 1
-#acumulador
-t = m = valor = subtrair = soma = 0
+#acumuladores
+t = m = valor = subtrair = somar = dividir = resto = resto_10 = 0
+somar_10 = multiplicar_10 = subtrair_1 = cont = final = 0
 while True:
     sacar = int(input('Quanto você quer sacar? '))
     total_1 = sacar // 50
-    while t < total_1:
-        t = sacar // cedula_c
-        valor = t * cedula_c
-        m = valor + 50
-        if m > sacar:
-            subtrair = valor - sacar
-            soma = subtrair + valor
-            if soma == sacar:
-                print(f'Total de {t} cédulas de R$50')
-                print(f'Total de {t}')
+    m = total_1 * cedula_c # é o valor que eu tenho até então (somando as notas de 50 necessárias)
+    somar = m + cedula_c # se o valor que eu tenho somado mais 50, passar do sacar, passa para as cedulas de 20
+    if somar > sacar:
+        subtrair = sacar - m # subtração para saber o valor da cédula necessário para chegar ao valor de sacar
+        if subtrair == 20:
+            dividir = subtrair // 20
+            print(f'Total de {total_1} de cédula R$50.')
+            print(f'Total de {dividir} de R$20.')
+        else:
+            if subtrair < 20:
+                resto_10 = subtrair // cedula_d
+                multiplicar_10 = resto_10 * 10
+                somar_10 = multiplicar_10 + m
+                print(f'Total de {total_1} cédulas de R$50.')
+                print(f'Total de {resto_10} cédulas de R$10.')
+                if somar_10 < sacar:
+                    subtrair_1 = sacar - somar_10
+                    while cont < subtrair_1:
+                        cont += 1
+                        final = cont
+                        print(f'Total de {total_1} cédulas de R$50.')
+                        print(f'Total de {resto_10} cédulas de 10R$.')
+                        print(f'Total de {final} cédulas de R$1.')
+    else:
+        print(f'Total de {total_1} de R$50')
     if sacar > 0:
         break
 print('FIM.')

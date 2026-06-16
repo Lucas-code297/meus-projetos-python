cec40e4946bd3f89e814dd21f4589fefb618d785
Lucas-code297 <lucash7z777@gmail.com>
2026-06-16 16:25:54 -0300

    ex071 - Simulador de Caixa Eletronico

--- Exercícios/ex071.py
+++ Exercícios/ex071.py
@@ -1,42 +1,36 @@
-print('=' * 25)
-print('BRANCO CEV')
-print('=' * 25)
-# cédulas
-cedula_c = 50
-cedula_v = 20
-cedula_d = 10
-cedula_u = 1
-#acumuladores
-t = m = valor = subtrair = somar = dividir = resto = resto_10 = 0
-somar_10 = multiplicar_10 = subtrair_1 = cont = final = 0
+print('=' * 30)
+print(f'{'BANCO CEV':^30}')
+print('=' * 30)
+
+sacar = int(input('Quanto você quer sacar? R$ '))
+
+total_cedulas = 0
+cedula_atual = 50
+
 while True:
-    sacar = int(input('Quanto você quer sacar? '))
-    total_1 = sacar // 50
-    m = total_1 * cedula_c # é o valor que eu tenho até então (somando as notas de 50 necessárias)
-    somar = m + cedula_c # se o valor que eu tenho somado mais 50, passar do sacar, passa para as cedulas de 20
-    if somar > sacar:
-        subtrair = sacar - m # subtração para saber o valor da cédula necessário para chegar ao valor de sacar
-        if subtrair == 20:
-            dividir = subtrair // 20
-            print(f'Total de {total_1} de cédula R$50.')
-            print(f'Total de {dividir} de R$20.')
-        else:
-            if subtrair < 20:
-                resto_10 = subtrair // cedula_d
-                multiplicar_10 = resto_10 * 10
-                somar_10 = multiplicar_10 + m
-                print(f'Total de {total_1} cédulas de R$50.')
-                print(f'Total de {resto_10} cédulas de R$10.')
-                if somar_10 < sacar:
-                    subtrair_1 = sacar - somar_10
-                    while cont < subtrair_1:
-                        cont += 1
-                        final = cont
-                        print(f'Total de {total_1} cédulas de R$50.')
-                        print(f'Total de {resto_10} cédulas de 10R$.')
-                        print(f'Total de {final} cédulas de R$1.')
+    # Verifica se o valor atual do saque consegue ser pago com a cédula atual
+    if sacar >= cedula_atual:
+        sacar -= cedula_atual
+        total_cedulas += 1
     else:
-        print(f'Total de {total_1} de R$50')
-    if sacar > 0:
-        break
-print('FIM.')
+        # Se contamos alguma cédula do valor atual, mostramos na tela
+        if total_cedulas > 0:
+            print(f'Total de {total_cedulas} cédulas de R${cedula_atual}')
+
+        # Mudamos para a próxima cédula disponível
+        if cedula_atual == 50:
+            cedula_atual = 20
+        elif cedula_atual == 20:
+            cedula_atual = 10
+        elif cedula_atual == 10:
+            cedula_atual = 1
+
+        # Zeramos o contador para a nova cédula
+        total_cedulas = 0
+
+        # Se o valor do saque chegou a 0, encerramos o programa
+        if sacar == 0:
+            break
+
+print('=' * 25)
+print('FIM DO PROGRAMA. Volte sempre!')

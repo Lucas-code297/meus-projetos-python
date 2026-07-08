04831266abddb361a9800d60c89d91118a697007
Lucas-code297 <lucash7z777@gmail.com>
2026-07-08 15:24:38 -0300

    ex074 - Maior e menor valor em uma tupla

--- Exercícios/ex074.py
+++ Exercícios/ex074.py
@@ -1,24 +1,24 @@
+# gerar 5 valores, colocá-los em uma tupla e dizer qual é o menor e qual é o menor
+
 from random import randint
-maior = menor = cont = 0
-tupla = []
-print(f'Valores sorteados:', end=' ')
+# contador
+cont = maior = menor = 0
+print(f'Gerados:', end=' ')
 while True:
-    tupla = [randint(0, 10)]
+    gerar = randint(0, 10)
     cont += 1
-    '''tupla = gerar'''
-    '''print('Gerados:', end=' ')'''
+    tupla = (gerar)
     print(tupla, end=' ')
     if cont == 1:
-        maior = tupla
-        menor = tupla
+        maior = gerar
+        menor = gerar
     else:
-        if tupla > maior:
-            maior = tupla
-        if tupla < menor:
-            menor = tupla
+        if gerar > maior:
+            maior = gerar
+        if gerar < menor:
+            menor = gerar
     if cont == 5:
         break
-'''print(tupla)'''
-print(f'\nMaior valor sorteado: {maior}')
-print(f'Menor valor sorteado: {menor}')
+print(f'\nMaior valor gerado: {maior}')
+print(f'Menor valor gerado: {menor}')
 print('Finalizado.')

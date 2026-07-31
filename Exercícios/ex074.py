9ae61d90802af845d11b220812ff399d2398d9ea
Lucas-code297 <lucash7z777@gmail.com>
2026-07-31 16:34:17 -0300

    ex074 - atualizando programa

--- Exercícios/ex074.py
+++ Exercícios/ex074.py
@@ -1,24 +1,12 @@
 # gerar 5 valores, colocá-los em uma tupla e dizer qual é o menor e qual é o menor
 
 from random import randint
-# contador
-cont = maior = menor = 0
-print(f'Gerados:', end=' ')
-while True:
-    gerar = randint(0, 10)
-    cont += 1
-    tupla = (gerar)
-    print(tupla, end=' ')
-    if cont == 1:
-        maior = gerar
-        menor = gerar
-    else:
-        if gerar > maior:
-            maior = gerar
-        if gerar < menor:
-            menor = gerar
-    if cont == 5:
-        break
-print(f'\nMaior valor gerado: {maior}')
-print(f'Menor valor gerado: {menor}')
-print('Finalizado.')
+# gerandos os 5 valores aleatórios dentro da tupla
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10),
+           randint(1, 10), randint(1, 10))
+print(f'Os 5 valores digitados foram: ', end='')
+# tirando os parênteses
+for n in numeros:
+    print(f'{n} ', end='')
+print(f'\nO maior valor sorteado foi: {max(numeros)}')
+print(f'O menor valor sorteado foi: {min(numeros)} ')
